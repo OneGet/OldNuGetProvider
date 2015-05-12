03f58985953736a08aae2c1772d667559c9955c8
Garrett Serack <garretts@microsoft.com>
2015-05-12 15:23:27 -0700

    fixed scripts

--- unregister.ps1
+++ unregister.ps1
@@ -1,9 +1,3 @@
-# make sure the outercurve package is installed
-libs\nuget.exe restore
-
 # make sure that we don't have a test version registered in the registry
 mkdir hkcu:software\Microsoft\PackageManagement -ea silentlycontinue
 Set-ItemProperty -Path hkcu:software\Microsoft\PackageManagement -Name NuGet -Value $null
-
-# call the MSBuild script
-& "C:\Program Files (x86)\MSBuild\12.0\Bin\amd64\MSBuild.exe" .\NuGetProvider.sln /p:Configuration=Release /p:Platform="Any CPU"
